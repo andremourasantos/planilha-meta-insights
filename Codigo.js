c48a5c1c23e0fd550f58e71c0ace776c80209a74
André <contato@andremourasantos.com.br>
2024-07-06 21:54:04 -0300

    v1.0.2 - Código mesclado com main branch; - Funcionalidades OK;

--- Codigo.js
+++ Codigo.js
@@ -1,3 +1,14 @@
+const ACTIVE_SPREADSHEET = SpreadsheetApp.getActiveSpreadsheet();
+const SCRIPT_LIBRARY_NAME = 'PlanilhaMetaInsights';
+const FACEBOOK_INSIGHTS_SHEET_NAME = 'Facebook Insights';
+const INSTAGRAM_INSIGHTS_SHEET_NAME = 'Instagram Insights';
+
+//Global sheet estilization
+const ROW_HEIGHT = 42;
+const COLUMN_WIDTH_P = 84;
+const COLUMN_WIDTH_M = COLUMN_WIDTH_P * 2;
+const COLUMN_WIDTH_G = COLUMN_WIDTH_P * 3;
+
 function onOpen(e) {
   addMenu();
 }
@@ -6,14 +17,174 @@ const ACTIVESPREADSHEET = SpreadsheetApp.getActiveSpreadsheet();
 const UI = SpreadsheetApp.getUi();
 
 function addMenu() {
+  const FIRST_STEPS_SUBMENU = UI
+    .createMenu('Primeiros passos')
+      .addItem('Criar Facebook Insights', `${SCRIPT_LIBRARY_NAME}.directToCreateFacebookSheet`)
+      .addItem('Criar Instagram Insights', `${SCRIPT_LIBRARY_NAME}.directToCreateInstagramSheet`)
   SpreadsheetApp.getUi()
-    .createMenu(' Automações')
-    .addItem('Importar Alcance', 'importReach')
-    .addItem('Importar Curtidas', 'importLikes')
-    .addItem('Importar Seguidores', 'importFollowers')
+    .createMenu('🤖 Assistente')
+    .addSubMenu(FIRST_STEPS_SUBMENU)
     .addSeparator()
-    .addItem('Sobre o script', 'aboutTheScript')
+    .addItem('Importar Alcance', `${SCRIPT_LIBRARY_NAME}.importReach`)
+    .addItem('Importar Curtidas', `${SCRIPT_LIBRARY_NAME}.importLikes`)
+    .addItem('Importar Seguidores', `${SCRIPT_LIBRARY_NAME}.importFollowers`)
+    .addSeparator()
+    .addItem('Sobre o script', `${SCRIPT_LIBRARY_NAME}.aboutTheScript`)
     .addToUi();
+}
+
+//Auxiliary functions
+/**
+ * Used in the menu items to direct the creation of the Facebook Insights sheet using the `createInsightsSheet` function.
+ *
+ */
+function directToCreateFacebookSheet() {
+  createInsightsSheet('Facebook');
+}
+
+/**
+ * Used in the menu items to direct the creation of the Instagram Insights sheet using the `createInsightsSheet` function.
+ *
+ */
+function directToCreateInstagramSheet() {
+  createInsightsSheet('Instagram');
+}
+
+/**
+ * Create an entire new sheet for the Facebook Insights data. If the sheet already exists, it returns a custom error alert.
+ *
+ * @returns Either "sheet_already_exists" if the sheet already exists or "sheet_created" if a new sheet is successfully created.
+ */
+function createInsightsSheet(platform) {
+  let sheet;
+  const SHEET_NAME = platform === 'Facebook' ? FACEBOOK_INSIGHTS_SHEET_NAME : INSTAGRAM_INSIGHTS_SHEET_NAME;
+
+  try {
+    sheet = ACTIVE_SPREADSHEET.insertSheet(SHEET_NAME);
+  } catch (error) {
+    showCustomErrorAlert('⚠️ Planilha já existente', `A planilha ${SHEET_NAME} já foi criada. Caso queria criá-la novamente, é necessário excluir a atual e executar essa ação novamente.`);
+
+    return 'sheet_already_exists';
+  }
+
+  ACTIVE_SPREADSHEET.toast(`Criando planilha ${SHEET_NAME}...`)
+  ACTIVE_SPREADSHEET.setActiveSheet(sheet);
+
+  const NUMBER_OF_ROWS = getNumberOfDaysInYear();
+
+  defineRowsAndColumnsWireframes(sheet, NUMBER_OF_ROWS, 6);
+  defineSheetTextAligment(sheet, 'right');
+  commomSheetEstilization(sheet);
+
+  const HEADER_TITLES = [['Nº do Mês', 'Mês', 'Data', 'Alcance', 'Curtidas', 'Seguidores']];
+
+  //TODO: MAKE THE defineRowsAndColumnsWireframes FUNCTION ACCEPT COLUMN WIDTHS AS A PARAMETER.
+  const HEADER_COLUMN_WIDTHS = [COLUMN_WIDTH_P, COLUMN_WIDTH_P, COLUMN_WIDTH_P, COLUMN_WIDTH_M, COLUMN_WIDTH_M, COLUMN_WIDTH_M];
+  
+  sheet.getRange(1,1,1,6).setValues(HEADER_TITLES);
+
+  populateDefaultValues(sheet);
+
+  return 'sheet_created';
+}
+
+/**
+ * Get the number of days in the current year.
+ *
+ * @return {number} the number of days.
+ */
+function getNumberOfDaysInYear() {
+  const CURRENT_YEAR = new Date().getFullYear();
+
+  const START_DATE = new Date(CURRENT_YEAR, 0, 1);
+  const END_DATE = new Date(CURRENT_YEAR, 11, 31);
+  let daysArray = [];
+
+  for (let currentDate = START_DATE; currentDate <= END_DATE; currentDate.setDate(currentDate.getDate() + 1)) {
+    daysArray.push(currentDate.getDate());
+  }
+
+  return daysArray.length;
+}
+
+/**
+ * Define the length (number of rows) and width (number of columns) of the sheet.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet A sheet object.
+ * @param {number} maxRows The number of rows the sheet should have.
+ * @param {number} maxColumns The number of columns the sheet should have.
+ */
+function defineRowsAndColumnsWireframes(sheet, maxRows, maxColumns) {
+  sheet.deleteColumns(maxColumns, sheet.getMaxColumns() - maxColumns);
+  sheet.deleteRows(maxRows, sheet.getMaxRows() - maxRows);
+  sheet.setRowHeightsForced(1, sheet.getMaxRows(), 42);
+}
+
+function defineSheetTextAligment(sheet, type) {
+  const ALL_SHEET = sheet.getRange(1,1,sheet.getMaxRows(),sheet.getMaxColumns());
+
+  ALL_SHEET
+    .setVerticalAlignment("middle")
+    .setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP)
+    .setHorizontalAlignment(type)
+}
+
+/**
+ * Apply the a pre-defined common sheet stylization to the entire sheet.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet A sheet object.
+ */
+function commomSheetEstilization(sheet) {
+  const ALL_SHEET = sheet.getRange(1,1,sheet.getMaxRows(),sheet.getMaxColumns());
+  const HEADER_STYLE = SpreadsheetApp.newTextStyle().setBold(true).build();
+
+  sheet.getRange(1,1,1,sheet.getMaxColumns()).setTextStyle(HEADER_STYLE);
+  ALL_SHEET.setBorder(true, true, true, true, true, true, "#999999", SpreadsheetApp.BorderStyle.SOLID);
+
+  ALL_SHEET
+    .setFontFamily('Atkinson Hyperlegible')
+    .applyRowBanding(SpreadsheetApp.BandingTheme.LIGHT_GREY);
+}
+
+/**
+ * Populate the default values and formulas into a defined sheet. The sheet must be a Facebook or Intagram Insights sheet.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet The sheet object to insert the default values.
+ */
+function populateDefaultValues(sheet) {
+  const SHEET_MAX_ROWS = sheet.getMaxRows();
+  const DATES_RANGE = sheet.getRange(2,3,SHEET_MAX_ROWS,1);
+  const MONTH_NAME_RANGE = sheet.getRange(2,2,SHEET_MAX_ROWS,1);
+  const MONTH_NUMBER_RANGE = sheet.getRange(2,1,SHEET_MAX_ROWS,1);
+
+  const getDatesFromCurrentYear = () => {
+    const CURRENT_YEAR = new Date().getFullYear();
+
+    const START_DATE = new Date(CURRENT_YEAR, 0, 1);
+    const END_DATE = new Date(CURRENT_YEAR, 11, 31);
+    let datesArray = [];
+
+    for (let currentDate = START_DATE; currentDate <= END_DATE; currentDate.setDate(currentDate.getDate() + 1)) {
+      datesArray.push([new Date(currentDate)]);
+    }
+    return datesArray;
+  };
+
+  DATES_RANGE.setValues(getDatesFromCurrentYear());
+
+  for(let i=0;i<SHEET_MAX_ROWS;i++) {
+    const CELL = MONTH_NAME_RANGE.getCell(i+1,1);
+    const CELL_ROW = CELL.getRow();
+
+    CELL.setFormula(`=PROPER(TEXT(C${CELL_ROW};"mmmm"))`);
+  };
+
+  for(let i=0;i<SHEET_MAX_ROWS;i++) {
+    const CELL = MONTH_NUMBER_RANGE.getCell(i+1,1);
+    const CELL_ROW = CELL.getRow();
+
+    CELL.setFormula(`=MONTH(C${CELL_ROW})`);
+  };
 }
 
 //Main functions
@@ -377,4 +548,14 @@ function getLastRowWithValue(columnToCheck) {
 
 function aboutTheScript() {
   Browser.msgBox('Sobre o script', 'Criado por André Moura Santos, contato@andremourasantos.com.br.', Browser.Buttons.OK)
+}
+
+/**
+ * Show a custom error message in the Browser message box style. Offers only a button to click "OK".
+ *
+ * @param {string} title The title of the message box.
+ * @param {string} msg The message to be shown in the message box.
+ */
+function showCustomErrorAlert(title, msg) {
+  Browser.msgBox(title, msg, Browser.Buttons.OK);
 }
